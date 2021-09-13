4f2194ffbadd9fef92396bb14fcc2ef03f6a0370
Manmohan Sharma In <manmohan.sharma.in@jasperindia.local>
2021-09-13 14:56:41 +0530

    started

---
+++ Spring_MVC_self/src/main/java/com/example/Spring_MVC_self/SpringMvcSelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_MVC_self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSelfApplication.class, args);
+	}
+
+}
---
+++ Spring_MVC_self/src/test/java/com/example/Spring_MVC_self/SpringMvcSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_MVC_self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
